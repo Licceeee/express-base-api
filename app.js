e4f7d270baf28b90963b1b07be960d52945ccaee
Alicia Schonefeld <schonefeld.dev@gmail.com>
2022-04-19 18:45:40 +0100

    add app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+require('express-async-errors');
+require('dotenv').config();
+require('./db/client');
+
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const app = express();
+const cors = require('cors');
+
+const userRouter = require('./routes/user.router');
+const authRouter = require('./routes/auth.router');
+
+app.use(cors({ exposedHeaders: 'x-authorization-token' }));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/v1', authRouter);
+app.use('/api/v1/user', userRouter);
+
+
+module.exports = app;
+
